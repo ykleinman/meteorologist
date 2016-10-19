62571c218c84440903a2217d5e18ceb6ac195359
Yaniv Kleinman <yaniv.kleinman@gmail.com>
2016-10-18 19:59:46 -0500

    Part 1 answer

--- app/controllers/geocoding_controller.rb
+++ app/controllers/geocoding_controller.rb
@@ -16,12 +16,19 @@ class GeocodingController < ApplicationController
     # A URL-safe version of the street address, with spaces and other illegal
     #   characters removed, is in the variable @street_address_without_spaces.
     # ==========================================================================
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + @street_address_without_spaces
 
+    open(url)
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
+    results = parsed_data["results"]
+    first = results[0]
+    geometry = first["geometry"]
+    location = geometry["location"]
 
+    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
 
-    @latitude = "Replace this string with your answer."
-
-    @longitude = "Replace this string with your answer."
+    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
 
     render("geocoding/street_to_coords.html.erb")
   end
