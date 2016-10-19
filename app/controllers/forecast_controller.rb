32f93c42e763640b3d738eb7d77ac8ea3d06e94e
Yaniv Kleinman <yaniv.kleinman@gmail.com>
2016-10-19 18:19:20 -0500

    changes2

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -16,11 +16,18 @@ class ForecastController < ApplicationController
     # The longitude the user input is in the string @lng.
     # ==========================================================================
 
+    url = "https://api.darksky.net/forecast/a69021676788c845c86c0cf3984a5926/37.8267,-122.4233"
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
+    currently = parsed_data["currently"]
+    minutely = parsed_data["minutely"]
+    summary_of_next_sixty_minutes = minutely["summary"]
+    hourly = parsed_data["hourly"]
+    summary_of_next_several_days = minutely["summary"]
 
+    @current_temperature =  temperature = currently["temperature"]
 
-    @current_temperature = "Replace this string with your answer."
-
-    @current_summary = "Replace this string with your answer."
+    @current_summary = summary = currently["summary"]
 
     @summary_of_next_sixty_minutes = "Replace this string with your answer."
 
